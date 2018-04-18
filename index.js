216cdbfa6f327b1368a1a299a5835e78d5a1d7cf
Noane Dan <Noane.Dan@outlook.com>
2018-04-18 19:05:28 +0300

    Add web app

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/motion', (req, res) => {
+
+  console.log('[+] Received motion data!');
+
+  let data = req.body;
+  let parsedData = '';
+
+  for (let m of data) {
+    let {id, accelerometer, gyroscope, magnetometer, position, activity} = m;
+
+    parsedData += `${accelerometer.x},${accelerometer.y},${accelerometer.z},${gyroscope.x},${gyroscope.y},${gyroscope.z},${magnetometer.x},${magnetometer.y},${magnetometer.z},${position.heading},${position.speed},${activity}\n`;
+  }
+
+  fs.appendFile('motionData.csv', parsedData, (err) => {
+    if (err) {
+      console.log('[-] Error when writing to file!');
+    }
+  });
+
+  res.status(200).end();
+});
+
+app.listen(3000, () => console.log('[*] Started listening on port 3000!'));
